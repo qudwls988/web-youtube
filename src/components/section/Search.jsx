3f58419c510d5a389aeb5c0e17f940b41db742cb
qudwls988 <qudwls642@gmail.com>
2025-03-17 12:45:04 +0900

    페이지 작업

--- src/components/section/Search.jsx
+++ src/components/section/Search.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Search() {
+
+  const [searchKeyword, setSearchKeyword] = useState('')
+
   return (
     <div id='search'>
         <div className='search__inner'>
@@ -14,6 +17,7 @@ function Search() {
                 autoComplete='off'
                 className='search__input'
                 placeholder='검색어를 입력해주세요'
+                onChange={e => setSearchKeyword(e.target.value)}
                 />
         </div>
 
